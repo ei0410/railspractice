cbc54d166b0e06f24f1856e3af5201b2a1b45448
EijiMORI <ei@EijiMORInoMacBook-Air.local>
2019-09-28 16:00:19 +0900

    add post function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,18 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order(content: :desc)
   end
 
   def show
     @post = Post.find_by(id: params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @post = Post.new(content: params[:content])
+    @post.save
+    redirect_to("/posts/index")
   end
 end
