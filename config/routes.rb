50ec276d70c61ee5feb972dcaf1bbfa55a4bc9cb
EijiMORI <ei@EijiMORInoMacBook-Air.local>
2019-09-28 15:23:14 +0900

    add page's details

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'posts/index' => "posts#index"
+  get 'posts/:id' => "posts#show"
   get '/' => "home#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'about' => "home#about"
