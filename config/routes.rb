a84dc6b31a7e4d130ff037c98a4477cadf3b60ab
EijiMORI <ei@EijiMORInoMacBook-Air.local>
2019-09-29 12:54:23 +0900

    add edit and destory

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'posts/new' => "posts#new"
   post 'posts/create' => "posts#create"
   get 'posts/:id' => "posts#show"
+  get 'posts/:id/edit' => "posts#edit"
+  post 'posts/:id/update' => "posts#update"
+  post 'posts/:id/destroy' => "posts#destroy"
   get '/' => "home#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'about' => "home#about"
