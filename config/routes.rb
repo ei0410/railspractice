99fcbc8ec3330352a22df626439cdf2730376d77
ei0410 <chronolavos0124@gmail.com>
2020-02-05 23:13:57 +0900

    add user edit and user update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  post 'users/:id/update' => 'users#update'
+  get 'users/:id/edit' => 'users#edit'
+  post 'users/create' => 'users#create'
+  get 'signup' => 'users#new'
   get 'users/:id/show' => "users#show"
   get 'users/index' => "users#index"
+
   get 'posts/index' => "posts#index"
   get 'posts/new' => "posts#new"
   post 'posts/create' => "posts#create"
@@ -8,7 +13,7 @@ Rails.application.routes.draw do
   get 'posts/:id/edit' => "posts#edit"
   post 'posts/:id/update' => "posts#update"
   post 'posts/:id/destroy' => "posts#destroy"
+
   get '/' => "home#top"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'about' => "home#about"
 end
