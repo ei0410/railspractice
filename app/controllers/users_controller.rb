41d87edaeb4fb66efd35fe13d48f814fd904d4ec
ei0410 <chronolavos0124@gmail.com>
2020-03-31 10:33:15 +0900

    fixed bug (session[user_id] = @user.id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(name: params[:name], email: params[:email], image_name: "default_user.jpg", password: params[:password])
     if @user.save
-      session[:user_id] = @user_id
+      session[:user_id] = @user.id
       flash[:notice] = "ユーザー登録が完了しました"
-      redirect_to("/users/#{@user.id}/show")
+      redirect_to("/users/#{@user.id}")
     else
       render("users/new")
     end
